a67f0142db3967d8481781f4721d02c37fee6d0a
Ryan <hiRyan@protonmail.com>
2018-12-18 20:15:03 +0200

    basic structure created

---
+++ src/main/java/com/sdagroup/gradleairbooking/GradleairbookingApplication.java
@@ -0,0 +1,14 @@
+package com.sdagroup.gradleairbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleairbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleairbookingApplication.class, args);
+	}
+
+}
+
