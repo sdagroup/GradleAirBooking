04a118b040c350eeb1d2173a71b2ace1e7b7dde1
Ryan <hiRyan@protonmail.com>
2019-01-04 22:17:38 +0200

    one to many database table structure setup

---
+++ src/main/java/com/sdagroup/gradleairbooking/entity/AddressEntity.java
@@ -0,0 +1,43 @@
+package com.sdagroup.gradleairbooking.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+/**
+ * Created by Ryan Alexander on 04/01/2019.
+ */
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="address")
+@EntityListeners(AuditingEntityListener.class)
+
+public class AddressEntity {
+
+    @Id
+//    .IDENTITY instead of AUTO because it stops it from creating an additional table
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @Column
+    private String street;
+
+    @Column String postalCode;
+
+    @Column
+    private String city;
+
+    @Column
+    private String country;
+
+    @ManyToOne(targetEntity = PropertyEntity.class)
+    @JoinColumn(name = "propertyID", referencedColumnName = "propertyId")
+    private PropertyEntity property;
+}
---
+++ src/main/java/com/sdagroup/gradleairbooking/entity/PropertyEntity.java
@@ -0,0 +1,54 @@
+package com.sdagroup.gradleairbooking.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Ryan Alexander on 04/01/2019.
+ */
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="property")
+@EntityListeners(AuditingEntityListener.class)
+
+public class PropertyEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long propertyId;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date modifiedAt;
+
+    @Column(unique = true, nullable = false)
+    private String propertyName;
+
+    @Column (precision = 10, scale = 2)
+    private BigDecimal startsFrom;
+
+//    captured by chunck size of 10
+    @BatchSize(size=10)
+    @OneToMany (targetEntity = AddressEntity.class, mappedBy = "property", cascade = CascadeType.ALL)
+    private List<AddressEntity> addresses;
+}
